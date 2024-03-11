81d8d890cac9f6251df689789ad02d9b1c457b62
Richard Hao <richard@0xdev.dev>
2024-03-11 15:52:59 +0800

    feat: create v2ray proxy service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use pingora::{proxy::http_proxy_service, server::Server};
+use pingora_gateway::services::v2ray::V2rayService;
+
+// RUST_LOG=INFO cargo run
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    //TODO: read command line arguments
+
+    let mut server = Server::new(None).unwrap();
+    server.bootstrap();
+
+    let mut v2ray_proxy = http_proxy_service(
+        &server.configuration,
+        V2rayService::new(
+            "one.one.one.one".to_string(),
+            "1.1.1.1:443".to_string(),
+            "/xyz".to_string(),
+            false,
+        ),
+    );
+    v2ray_proxy.add_tcp("0.0.0.0:8999");
+
+    let mut v2ray_proxy_2 = http_proxy_service(
+        &server.configuration,
+        V2rayService::new(
+            "one.one.one.two".to_string(),
+            "1.1.1.1:443".to_string(),
+            "/abc".to_string(),
+            false,
+        ),
+    );
+    v2ray_proxy_2.add_tcp("0.0.0.0:8999");
+
+    server.add_service(v2ray_proxy);
+    server.add_service(v2ray_proxy_2);
+    server.run_forever();
 }
---
+++ src/services/v2ray.rs
@@ -0,0 +1,93 @@
+use async_trait::async_trait;
+use pingora::{
+    http::{ResponseHeader, StatusCode},
+    proxy::{ProxyHttp, Session},
+    upstreams::peer::HttpPeer,
+};
+
+pub struct V2rayService {
+    host: String,    // e.g. one.one.one.one
+    address: String, // e.g. 127.0.0.1:10086
+    ws_path: String, // e.g. /ray
+    is_tls: bool,
+}
+
+#[async_trait]
+impl ProxyHttp for V2rayService {
+    type CTX = ();
+
+    fn new_ctx(&self) -> Self::CTX {}
+
+    async fn request_filter(
+        &self,
+        session: &mut Session,
+        _ctx: &mut Self::CTX,
+    ) -> pingora::Result<bool>
+    where
+        Self::CTX: Send + Sync,
+    {
+        let request_path = session.req_header().uri.path();
+
+        if request_path == "/" {
+            let mut resp_header = ResponseHeader::build(StatusCode::OK, None)?;
+            resp_header.insert_header("Server", "Cloudflare")?;
+            session.set_keepalive(None);
+            session.write_response_header_ref(&resp_header).await?;
+            session.write_response_body("Connecting...".into()).await?;
+
+            // true: early return as the response is already written
+            return Ok(true);
+        }
+
+        if !self.check_ws_path(request_path) {
+            session.respond_error(404).await;
+
+            // true: early return as the response is already written
+            return Ok(true);
+        }
+
+        Ok(false)
+    }
+
+    async fn upstream_peer(
+        &self,
+        _session: &mut Session,
+        _ctx: &mut Self::CTX,
+    ) -> pingora::Result<Box<HttpPeer>> {
+        let peer = Box::new(HttpPeer::new(&self.address, self.is_tls, self.host.clone()));
+        Ok(peer)
+    }
+
+    async fn response_filter(
+        &self,
+        _session: &mut Session,
+        upstream_response: &mut ResponseHeader,
+        _ctx: &mut Self::CTX,
+    ) -> pingora::Result<()>
+    where
+        Self::CTX: Send + Sync,
+    {
+        // replace existing header if any
+        upstream_response.insert_header("Server", "Cloudflare")?;
+
+        // because pingora doesn't support h3
+        upstream_response.remove_header("alt-svc");
+
+        Ok(())
+    }
+}
+
+impl V2rayService {
+    pub fn new(host: String, address: String, ws_path: String, is_tls: bool) -> Self {
+        Self {
+            host,
+            address,
+            ws_path,
+            is_tls,
+        }
+    }
+
+    fn check_ws_path(&self, path: &str) -> bool {
+        path.starts_with(&self.ws_path)
+    }
+}
